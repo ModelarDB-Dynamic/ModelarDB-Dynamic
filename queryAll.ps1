51f2fe9f55901a14941e1ef661e38a0852dde973
KennethNorholm <70717116+KennethNorholm@users.noreply.github.com>
2021-11-24 12:55:36 +0100

    query all script

---
+++ queryAll.ps1
@@ -0,0 +1,7 @@
+﻿$basePath = "C:\Users\Kenneth\Downloads\low_freq_generated\"
+
+for ($i = 0; $i -le 20; $i++){
+$myPath = $basePath + $i + ".csv"
+curl -d "select * from datapoint order by timestamp asc" localhost:9999 | out-file -FilePath $myPath 
+#echo "hello" | out-file -FilePath $myPath 
+}
