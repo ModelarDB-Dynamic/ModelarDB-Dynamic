7a882457ed665938bef70e376e9fac672d3d9f4b
EsbenNedergaard <esben.nedergaard@yahoo.dk>
2021-11-29 11:33:18 +0100

    Fixed scaling factor problem for data point view

--- src/main/java/dk/aau/modelardb/core/utility/ValueFunction.java
+++ src/main/java/dk/aau/modelardb/core/utility/ValueFunction.java
@@ -18,6 +18,6 @@ import java.io.Serializable;
 
 public class ValueFunction implements Serializable {
     public float transform(float value, float scalingFactor) {
-        return value / scalingFactor;
+        return value; //TODO(EKN): discuss with SØREN how scaling factor should be used.
     }
 }
